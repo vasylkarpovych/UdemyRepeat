0ce27e0253be219dd19ee67965e06ac57df9a73f
vasylKarpovich <karpovichvasya@gmail.com>
2024-07-22 12:43:57 +0300

    add try{} catch(err){}

--- Udemy/exercise-106/script.js
+++ Udemy/exercise-106/script.js
@@ -1,22 +1,39 @@
-'use strict';
+"use strict";
 
 const data = [
-    {
-        id: 'box',
-        tag: 'div'
-    },
-    {
-        id: '',
-        tag: 'nav'
-    },
-    {
-        id: 'circle',
-        tag: 'span'
-    }
-]
+  {
+    id: "box",
+    tag: "div",
+  },
+  {
+    id: "fff",
+    tag: "nav",
+  },
+  {
+    id: "circle",
+    tag: "",
+  },
+];
 
-data.forEach(blockObj => {
+try {
+  data.forEach((blockObj, i) => {
     const block = document.createElement(blockObj.tag);
-    block.setAttribute('id', blockObj.id);
+
+    if (!blockObj.id)
+      throw new SyntaxError(
+        `В даних під номером ${i + 1} немає унікального id`
+      );
+
+    block.setAttribute("id", blockObj.id);
     document.body.append(block);
-})
+  });
+} catch (e) {
+  if (e.name === "SyntaxError") {
+    console.log(e.message);
+  } else {
+    throw e;
+  }
+}
+
+const err = new SyntaxError("dsfnsdjkfhjksdhf");
+console.log(err.name, err.message, err.stack);
